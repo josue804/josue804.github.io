5c571da1e58518241daecb63a79cc712fefdc766
Josue Valverde <josuevalverde12@gmail.com>
2015-12-13 14:55:06 -0800

    rwliejra

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,9 +15,17 @@ end
 Dir.glob("#{Rails.root}/app/assets/stylesheets/**/").each do |path|
   Rails.application.config.assets.paths << path
 end
-Dir.glob("#{Rails.root}/app/assets/vendor/**").each do |path|
+Dir.glob("#{Rails.root}/app/assets/vendor/**/**").each do |path|
   Rails.application.config.assets.paths << path
 end
-Rails.application.config.assets.precompile += %w(*.css)
+
+Rails.application.config.assets.precompile += %w( custom.css )
+Rails.application.config.assets.precompile += %w( demo.css )
+Rails.application.config.assets.precompile += %w( home.css )
+Rails.application.config.assets.precompile += %w( responsive.css )
+Rails.application.config.assets.precompile += %w( rs-plugin-styles.css )
+Rails.application.config.assets.precompile += %w( theme-base.css )
+Rails.application.config.assets.precompile += %w( theme-elements.css )
+Rails.application.config.assets.precompile += %w(*.css, *.scss)
 Rails.application.config.assets.precompile += %w(*.js)
 Rails.application.config.assets.precompile += %w(*.jpg, *.png, *svg)
